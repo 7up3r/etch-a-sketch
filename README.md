bdace70f4665186905dfe68eddac9ee9e2785c93
7up3r <pankaj.th73@gmail.com>
2023-06-24 18:16:08 +0530

    added functinality to the the grid
    Added code to create a 16 X 16 grid and change color on hover effect
    on grid. tried to minimize the use of style.css and do most of the
    things from script.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # etch-a-sketch
 DOM manipulation project (TOP)
+
+This is project to, as decribed by TOP "to flex your DOM manipulation skills."
+
+It is something between a sketchpad and Etch-A-Sketch.
--- script.js
+++ script.js
@@ -1,12 +1,41 @@
+const containerDimension = "500";
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; ++i)
-{
-    const row = document.createElement(`div`);
-    container.appendChild(row);
-    row.classList.add('row');
-    for (let j = 0; j < 16; ++j){
-        const column = document.createElement(`div`);
-        row.appendChild(column)
-        column.classList.add('column');
+container.style.width = containerDimension + "px";
+container.style.height= containerDimension + "px";
+
+function createGrid(grid) {
+    for (let i = 0; i < grid; ++i) {
+        const row = document.createElement(`div`);
+        container.appendChild(row);
+        row.classList.add('row');
+        for (let j = 0; j < grid; ++j){
+            const column = document.createElement(`div`);
+            column.style.width = (containerDimension / grid) + "px";
+            column.style.height= (containerDimension / grid) + "px";
+            column.style.backgroundColor= "white";
+            column.style.boxSizing = "border-box";
+            row.appendChild(column)
+            column.classList.add('column');
+        }
     }
+    changeColorOnHover();
 }
+
+function changeColorOnHover() {
+    let items = document.querySelectorAll('.column');
+    items.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = `${randomColors()}`;
+        });    
+    });
+}
+
+function randomColors() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; ++i) {
+        color += letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+createGrid(100);
